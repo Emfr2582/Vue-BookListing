3c0fb0dce0e154715de866a1bb55f06c0beea7f9
Emilia <emilia.franken@gmail.com>
2019-05-10 14:07:06 +0200

    first setup done

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,13 +1,23 @@
 <template>
-    
+    <div>
+        <h1>{{title}}</h1>
+    </div>
 </template>
 
 <script>
 export default {
-    
+    name: 'Booklist',
+    data(){
+        return {
+            title: 'All books'
+        }
+    }
 }
 </script>
 <style scoped>
+h1,h2{
+    font-weight: normal
+}
 
 </style>
 
